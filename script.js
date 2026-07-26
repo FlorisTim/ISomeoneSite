4c2bcf7dc981737518e75a248441d58b9c1be9b8
Floris Timmermans <floris.timmermans@sintlucasedu.nl>
2026-07-27 00:14:45 +0200

    reworked the website

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function Main() {
 
     for (let i = 0; i < (window.screen.width < 900 ? 1 : 2); i++) {
         let entry = entries[i];
-        posts.innerHTML += (`<div class="slot bg">${generateYTEmbed(entry.querySelectorAll("id")[0].textContent)}</div>`);
+        posts.innerHTML += (`${generateYTEmbed(entry.querySelectorAll("id")[0].textContent)}`);
     }
     if(window.screen.width < 900){
         document.getElementsByClassName("replacement")[0].innerText = "Latest youtube video"
@@ -28,7 +28,7 @@ function generateYTEmbed(link) {
     if (link.includes("v=")){
         link = link.substring(link.lastIndexOf("v=") + 1);
     }
-    return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${link}"
+    return `<iframe src="https://www.youtube.com/embed/${link}"
                    title="YouTube video player"
                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                    referrerPolicy="strict-origin-when-cross-origin"
