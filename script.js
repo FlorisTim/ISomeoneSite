aad670402ba35a29786e7f7a6905c26fb1497f6c
Floris Timmermans <flotim20093@gmail.com>
2026-08-04 17:44:39 +0200

    added votings on my posts

--- script.js
+++ script.js
@@ -3,25 +3,104 @@ let index = 0;
 let posts = [];
 let postsElement
 
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+const supabase = createClient(
+    "https://lmgnsphsbfjjivyaoadq.supabase.co",
+    "sb_publishable_oYu_jtRndODulzdSprQVvA_FCDe9qAn"
+);
+
 const MOBILE = screen.width < 800;
 
 async function Main() {
     await lazyGrabPosts();
-    posts.reverse();
+    posts.reverse()
     console.log(posts)
     postsElement = document.getElementsByClassName("posts")[0]
-    generate(2);
+    await generate(2);
 }
 
-function generate(amount){
+async function likeButtons(id) {
+    id = -id + posts.length;
+    const voted = localStorage.getItem("voted" + id) !== null;
+    const { data } = await supabase
+        .from("Post Votes")
+        .select("thumbsup, thumbsdown")
+        .eq("id", id)
+        .maybeSingle();
+
+    const down = data?.thumbsdown ?? 0;
+    const up = data?.thumbsup ?? 0
+    return `
+     <div class="inline">
+                <div class="column">
+                <div class="interaction">
+                    <svg class="arrow ${voted ? '' : 'arrowUp'}" viewBox="0 0 8.544 10.716" ${voted ? '' : 'onclick="like(' + id + ')"'}">
+                        <path fill="currentColor" d="M4.272 0l4.272 4.395H5.72v6.321H2.824V4.395H0z"/>
+                    </svg>
+                    <div class="forcerow text85 arrowUp" title="${up} people liked this">${!voted ? "" : up}</div>
+                </div>
+                </div>
+                <div class="column">
+                <div class="interaction">
+                    <svg viewBox="0 0 8.544 10.716" class="arrow ${voted ? '' : 'arrowDown'}"   ${voted ? '' : 'onclick="disLike(' + id + ')"'}">
+                        <path fill="currentColor" d="M0 6.321h2.824V0h2.896v6.321h2.824L4.272 10.716z"/>
+                    </svg>
+                    <div class="forcerow text85 arrowDown" title="${down} people disliked this">${!voted ? "" : down}</div>
+                </div>
+                
+                </div>
+            </div>
+    `
+}
+
+window.like = like;
+window.disLike = disLike;
+
+async function like(id){
+    localStorage.setItem("voted" + id, "true");
+    const { data, error } = await supabase.from("Post Votes").select("id, thumbsup").eq("id",id).maybeSingle();
+    if (data){
+        await supabase.from("Post Votes").update({thumbsup: data.thumbsup + 1}).eq("id",id);
+    } else {
+        await supabase.from("Post Votes").insert({
+            id: id,
+            thumbsup: 1,
+            thumbsdown: 0
+        })
+    }
+    await refreshPost(id);
+}
+
+async function disLike(id){
+    localStorage.setItem("voted" + id, "true");
+    const { data, error } = await supabase.from("Post Votes").select("id, thumbsdown").eq("id",id).maybeSingle();
+    if (data){
+        await supabase.from("Post Votes").update({thumbsdown: data.thumbsdown + 1}).eq("id",id);
+    } else {
+        await supabase.from("Post Votes").insert({
+            id: id,
+            thumbsup: 0,
+            thumbsdown: 1
+        })
+    }
+    await refreshPost(id);
+}
+
+async function generate(amount){
     for (let i = 0; i < amount; i++){
-        postsElement.innerHTML += parseDoc(posts[index]);
+        postsElement.innerHTML += await parseDoc(posts[index],index);
         index++;
         if (index >= posts.length){
             document.getElementsByClassName("delete1")[0].remove()
             postsElement.innerHTML += `<div class="center graytext">You have reached the end</div>`;
         }
     }
+}
+
+async function refreshPost(id){
+    id = -id + posts.length;
+    document.getElementById("post_"+id).innerHTML = await parseDoc(posts[id],id);
 }
 
 async function lazyGrabPosts(){
@@ -55,7 +134,7 @@ function generateYTEmbed(link) {
                    allowFullScreen></iframe>`
 }
 
-function generatePost(Date, Title, Classes, Contents, MetaData) {
+async function generatePost(Date, Title, Classes, Contents, MetaData, index) {
     if (!MOBILE){
         Contents =             Contents.replaceAll("[https://www.youtube.com/watch?v=","<iframe src=\"https://www.youtube.com/embed/")
             .replaceAll("]yt",`\" title="YouTube video player"
@@ -72,7 +151,7 @@ function generatePost(Date, Title, Classes, Contents, MetaData) {
         }
 
     }
-    return `<div class="entry post column">
+    return `<div id="post_${index}"><div class="entry post column">
         <div class="title">${Title}
             <div class="graytext">${Date}</div>
         </div>
@@ -80,17 +159,21 @@ function generatePost(Date, Title, Classes, Contents, MetaData) {
         <div class="entry ${Classes}">
             ${Contents}
         </div>
-    </div>`
+        ${await likeButtons(index)}
+    </div></div>`
 }
 
+window.generate = generate;
+window.index = index;
 
-function parseDoc(Document){
-    return generatePost(
+async function parseDoc(Document,index){
+    return await generatePost(
         getUnsafe("date", Document),
         getUnsafe("title",Document),
         getUnsafe("class",Document),
         Document.substring(Document.indexOf("~")+1),
-        Document.substring(0,Document.indexOf("~"))
+        Document.substring(0,Document.indexOf("~")),
+        index
     );
 }
 
