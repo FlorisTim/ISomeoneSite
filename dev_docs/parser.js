a46967f1c1e05e275713bd9d730e1bd77d56ec0e
Floris Timmermans <floris.timmermans@sintlucasedu.nl>
2026-08-10 16:08:47 +0200

    full custom system works now

--- dev_docs/parser.js
+++ dev_docs/parser.js
@@ -19,14 +19,52 @@ let plans = false;
 
 let trim = false;
 
+let cssTemplate = `
+.gray{
+  color:$1;
+}
+th{
+    background-color: $3;
+}
+td, .post{
+  background-color: $5;
+}
+kbd{
+  background-color: $4;
+
+}
+.spckbd kbd {
+    background: linear-gradient(90deg, $4 0%, rgba(0, 0, 0, 0) 100%);
+    border: 1px solid $4 ;
+}
+body{
+    background-color: $2;
+}
+.sub {
+    background-color: $3;
+}
+a, .qlink{
+    color: $0;
+}
+a:hover, .qlink:hover{
+    color:$1;
+}
+.post .title, .post_section{
+    background-color:$4;
+}
+*{
+    scrollbar-color: $4 $3;
+}`
+
 async function act(path){
     await load();
 
     documentBody.innerHTML = await parse(path);
+    documentBody.innerHTML += `<style>${cssTemplate}</style>`;
 
     await main(youtubeToken,playlist,plans,webPosts,youtubePosts)
 }
-
+const hex = "0123456789abcdef";
 async function parseMetadata(header){
     for (let i = 0; i < header.length; i++) {
         switch (header[i]) {
@@ -51,8 +89,34 @@ async function parseMetadata(header){
             case "youtubeToken":
                 youtubeToken = read(i);
                 break;
+            case "theme":
+                const theme = read(i).split(" ");
+                cssTemplate = cssTemplate.replaceAll("$0", hexCode(theme,0));
+                cssTemplate = cssTemplate.replaceAll("$1", hexCode(theme,1));
+                cssTemplate = cssTemplate.replaceAll("$2", hexCode(theme,8));
+                cssTemplate = cssTemplate.replaceAll("$3", hexCode(theme,9));
+                cssTemplate = cssTemplate.replaceAll("$4", hexCode(theme,10));
+                cssTemplate = cssTemplate.replaceAll("$5", hexCode(theme,11));
+                break;
         }
     }
+
+    function hexCode(theme,index){
+        let red = theme[0]
+        let green = theme[1]
+        let blue = theme[2]
+
+        red -= index;
+        green -= index;
+        blue -= index;
+
+        red = Math.max(red, 0);
+        green = Math.max(green, 0);
+        blue = Math.max(blue, 0);
+        console.log(`#${index} = #${hex[red]}${hex[green]}${hex[blue]}`)
+        return `#${hex[red]}${hex[green]}${hex[blue]}`
+    }
+
     function read(i){
         i++
         if (header[i++] === "in_commons"){
